8c41d03a508a4e517415e0a9a5306fc5fe7d296b
soffiayanchashvili <119891979+soffiayanchashvili@users.noreply.github.com>
2023-01-16 08:44:06 -0800

    javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let user= "Mariami";
+let surName="Machavariani";
+let age= 18;
+let isFemale= true;
+let car= null;
+
+console.log(`${user} ${surName} ${age}`)
+
+let x=7;
+let y=5;
+
+let plus= x + y;
+let minus= x - y;
+let area= x * y;
+let split= x / y;
+
+console.log(plus)
+console.log(minus)
+console.log(area)
+console.log(split)
